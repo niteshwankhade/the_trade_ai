c1c6ad889c70fe25e1e19873ff37661695e481d2
niteshwankhade <niteshw228@gmail.com>
2026-02-04 09:59:34 +0530

    Update html.py

--- html.py
+++ html.py
@@ -1,5 +1,11 @@
 import yfinance as yf
 import pandas as pd
 import streamlit as st
+st.title("मेरा पहला ट्रेडिंग ऐप")
+st.write("अगर आप यह देख पा रहे हैं, तो इसका मतलब है कि आपकी वेबसाइट लाइव हो गई है!")
+
+# एक छोटा सा बटन और मैसेज
+if st.button('यहाँ क्लिक करें'):
+    st.success("बधाई हो! आपका ऐप सही से काम कर रहा है।")
 
 
